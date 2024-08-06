4e2322f42c97f9f0e2c527af1bf0bb7ff69bc6cf
Diogo Rodrigues <diogorodrigues699@yahoo.com>
2024-08-06 18:43:47 -0300

    criado api para criação de produtos e categorias e listagem das categorias

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,30 @@
 import { Router } from 'express';
-
+import multer, { Multer } from 'multer';
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload('./tmp'))
+
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/user-info', isAuthenticated, new DetailUserController().handle)
+
+// -- ROTAS CATEGORY --
+router.post('/category',isAuthenticated, new CreateCategoryController().handle)
+router.get('/category',isAuthenticated, new ListCategoryController().handle)
+
+
+// -- ROTAS PRODUTOS -- 
+router.post('/product',isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router }; 
